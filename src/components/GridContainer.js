e410617fddd9ff9ce71ad25d59630880ddf5d033
iZeeM <izeem@iZeeMs-iMac.local>
2020-12-24 13:13:27 +0800

    feat: toggle weight in sections

--- src/components/GridContainer.js
+++ src/components/GridContainer.js
@@ -14,12 +14,15 @@ class GridContainer extends Component{
         {
             'compactorSection' : 'A', 
             'compactorData' : [],
+            'count' : 0,
             'compactorLoaded' : false,
-            'renderWeightInformation' : false
+            'renderWeightInformation' : false,
+            'pressLeftArrowWeight' : false
         }
         this.renderWeightInformation = this.renderWeightInformation.bind(this)
         this.switchCompactorSection = this.switchCompactorSection.bind(this)
-        this.getCompactorSectionData = this.getCompactorSectionData.bind(this)
+        this.toggleRightArrow = this.toggleRightArrow.bind(this)
+        this.toggleLeftArrow = this.toggleLeftArrow.bind(this)
     }
 
     componentDidMount(){
@@ -28,7 +31,7 @@ class GridContainer extends Component{
         var config = {
             headers: { Authorization: `Bearer ${token}` }
         }
-        axios.get(`http://ec2-18-191-176-57.us-east-2.compute.amazonaws.com/allCompactorInfo/${currentSection}`,config)
+        axios.get(`http://ec2-18-191-176-57.us-east-2.compute.amazonaws.com/allCompactorInfo`,config)
         .then((response)=> {
             this.setState({
             compactorData : response.data.compactorInfo,
@@ -40,30 +43,32 @@ class GridContainer extends Component{
       })  
     }
 
+    reduceFunc(total, num){
+        return total + num
+    }
+
     switchCompactorSection(){
         this.setState({
             compactorSection : 'B',
         })
     }
 
-    getCompactorSectionData(){
-        var currentSection = this.state.compactorSection
-        console.log(currentSection)
-        var token = this.props.location.state.token
-        var config = {
-            headers: { Authorization: `Bearer ${token}` }
-        }
-        axios.get(`http://ec2-18-191-176-57.us-east-2.compute.amazonaws.com/allCompactorInfo/${currentSection}`,config)
-        .then((response)=> {
-            console.log(response.data.compactorInfo)
-            this.setState({
-            compactorData : response.data.compactorInfo,
-            compactorLoaded: true
-            })
-        })
-      .catch(function (error) {
-        console.log(error);
-      })
+    toggleRightArrow(){
+        var sections = ['A','B']
+        var count = this.state.count
+        count = count -1
+        this.setState({count : count})
+        var currentSection = sections[count]
+        this.setState({compactorSection : currentSection, pressRightArrowWeight : true, pressLeftArrowWeight : false})
+    }
+
+    toggleLeftArrow(){
+        var sections = ['A','B']
+        var count = this.state.count
+        count = count + 1
+        this.setState({count : count})
+        var currentSection = sections[count]
+        this.setState({compactorSection : currentSection, pressRightArrowWeight : false, pressLeftArrowWeight : true})
     }
 
     renderWeightInformation(){
@@ -76,35 +81,153 @@ class GridContainer extends Component{
 
     render(){    
         if(this.state.compactorLoaded){
-            let compactors = this.state.compactorData
-
-            var collectionWeights = []
-            function reduceFunc(total, num) {
-                return total + num;
+            var compactors = this.state.compactorData
+            if(this.state.pressLeftArrowWeight || this.state.pressRightArrowWeight){
+                var currentSection = this.state.compactorSection
+            }else{
+                var currentSection = 'A'
             }
+
+            var compactorData = []
+            var collectionWeights = []
+            for(var i=0;i<compactors.length;i++){
+                if(compactors[i].sectionArea == currentSection){
+                    compactorData.push(compactors[i])
+                }
+            }
+            compactors = compactorData
+
+            let reduceFunc = this.reduceFunc
 
             for(var i=0;i<compactors.length;i++){
                 if(!(isNaN(compactors[i].current_weight) && compactors[i].current_weight <= 0) ){
                     collectionWeights.push(compactors[i].current_weight)
                 }
             }
+
             var totalCollectedWeight = collectionWeights.reduce(reduceFunc);
 
-            var compactorInfo = compactors.map(compactor => (
-                <tr>
-                     <th>{compactor.compactorID}</th>
-                     <th>{compactor.current_weight}</th>
-                     <th>{compactor.max_weight}</th>
-                     <th>{(Math.round((compactor.current_weight / compactor.max_weight)*100 )) + ' %'}</th>
-                     <th>{Math.round((compactor.current_weight / compactor.max_weight)*100 ) < 25 ? 'green' : 
-                     Math.round((compactor.current_weight / compactor.max_weight)*100 ) < 50 ? 'yellow' : 'red'
-                     }</th>
-                     
-                 </tr>
-           ))
+            var compactorInfo = <tr><th>Loading ......</th></tr>
+            if(this.state.renderWeightInformation){
+                compactorInfo = compactors.map(compactor => (
+                    <tr>
+                         <th>{compactor.compactorID}</th>
+                         <th>{compactor.current_weight}</th>
+                         <th>{compactor.max_weight}</th>
+                         <th>{(Math.round((compactor.current_weight / compactor.max_weight)*100 )) + ' %'}</th>
+                         <th>{Math.round((compactor.current_weight / compactor.max_weight)*100 ) < 25 ? 
+                            <span><img
+                            src={require('./greendot.png')}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                            alt="React Bootstrap logo"
+                            /></span> : 
+                         Math.round((compactor.current_weight / compactor.max_weight)*100 ) < 50 ? 
+                            <span><img
+                            src={require('./yellowdot.png')}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                            alt="React Bootstrap logo"
+                            /></span> : 
+                            <span><img
+                            src={require('./reddot.png')}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                            alt="React Bootstrap logo"
+                            /></span>
+                         }</th>
+                         
+                     </tr>
+               ))
+            }
 
           }
 
+          if(this.state.renderWeightInformation){
+            return(
+                <div className='grid-container-compactor'>
+                <div className='grid-item grid-item-01-compactor'>
+                    <NavBarContent userType={this.props.location.state.userType} handleRedirect={this.handleRedirect} token={this.props.location.state.token} />
+               </div>
+               <div className="grid-item grid-item-sideDashboard whiteBG">
+                       <Container className="blueBG adjustPadding">
+                           <Row>
+                               <Col></Col>
+                               <Col>Dashboard</Col>
+                               <Col></Col>
+                           </Row>
+                       </Container>
+                       <Container className="blueBorder adjustPaddingContent">
+                           <Row>
+                               <Col>Map</Col>
+                           </Row>
+                       </Container>
+                       <Container className="blueBorder adjustPaddingContent">
+                           <Row>
+                               <Col>Equipment</Col>
+                           </Row>
+                       </Container>
+                       <Container className="blueBorder adjustPaddingContent">
+                           <Row>
+                               <Col>Report</Col>
+                           </Row>
+                       </Container>
+                       <Container className="blueBorder adjustPaddingContent">
+                           <Row>
+                               <Col>Admin</Col>
+                           </Row>
+                       </Container>
+               </div>
+               <div className="grid-item grid-item-alarmDashboard whiteBG">
+                       <Container>
+                           <Row>
+                               <Col className='alarmTitle'>Alarm</Col>
+                           </Row>
+                       </Container>
+                       <Container>
+                           <Row>
+                               <Col className='alarmRaisedNumber'>5</Col>
+                           </Row>
+                       </Container>
+                       <button>Status</button>
+                       <button>Status</button>
+                       <button>Status</button>
+               </div>
+               <div className="grid-item grid-item-weightDashboard whiteBG">
+                    <div>
+                        <div>&nbsp;</div>
+                        <Container>
+                        <Row>
+                            <Col style={{textAlign: 'center', fontSize : '1.4em'}}>Collection Weight</Col>
+                        </Row>
+                        </Container>
+                        <div>&nbsp;</div>
+                    </div>
+                    <Table striped bordered hover responsive> 
+                        <thead>
+                        <tr>
+                            <th>Compactor ID</th>
+                            <th>Collection Weight</th>
+                            <th>Max Load</th>
+                            <th>Weight Percentage</th>
+                            <th>Level</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {compactorInfo}
+                        </tbody>
+                    </Table>
+                    <button onClick={()=>{this.setState({renderWeightInformation: false})}}>Back</button>
+               </div>
+               <div className="grid-item grid-item-mapDashboard">
+                   <Mapping selectedAddress={this.state.selectedAddress} compactorFilledLevel={this.state.compactorFilledLevel} token={this.props.location.state.token} />
+               </div>
+           </div>
+            )
+            }else{
                 return(
                     <div className='grid-container-compactor'>
                          <div className='grid-item grid-item-01-compactor'>
@@ -162,23 +285,26 @@ class GridContainer extends Component{
                                 </Container>
                                 <Container>
                                     <Row>
+                                         {/* <Col className='alarmRaisedNumber'>1000</Col> */}
                                 <Col className='alarmRaisedNumber'>{totalCollectedWeight}</Col>
                                     </Row>
                                 </Container>
                                 <Container>
                                     <Row>
-                                        <Col style={{textAlign : 'center', cursor:'pointer'}}><FontAwesomeIcon icon={faArrowLeft} /></Col>
+                                        <Col onClick={()=>{this.toggleRightArrow()}} style={{textAlign : 'center', cursor:'pointer'}}><FontAwesomeIcon icon={faArrowLeft} /></Col>
                                         <Col style={{textAlign : 'center'}}>Section {this.state.compactorSection}</Col>
-                                        <Col onClick={()=>{this.switchCompactorSection(); this.getCompactorSectionData()}} style={{textAlign : 'center', cursor:'pointer'}}><FontAwesomeIcon icon={faArrowRight} /></Col>
+                                        <Col onClick={()=>{this.toggleLeftArrow()}} style={{textAlign : 'center', cursor:'pointer'}}><FontAwesomeIcon icon={faArrowRight} /></Col>
                                     </Row>
                                 </Container>
-                                <FontAwesomeIcon style={{cursor:'pointer', fontSize: '1.4em'}}icon={faBalanceScale} />
+                                <div>&nbsp;</div>
+                                <button onClick={this.renderWeightInformation}>Collection Weight</button>
                         </div>
                         <div className="grid-item grid-item-mapDashboard">
                             <Mapping selectedAddress={this.state.selectedAddress} compactorFilledLevel={this.state.compactorFilledLevel} token={this.props.location.state.token} />
                         </div>
                     </div>
                 )
+            }
     }
 }
 
